089291b5682c929b3c63b66a0eb460681119374e
Peter Lukic <peter.lukic@gmail.com>
2020-10-30 16:46:51 +0100

    Add ApiRestSharp Solution

---
+++ ApiRestSharp/ApiRestSharp/Features/GetPosts.feature
@@ -0,0 +1,8 @@
+﻿Feature: GetPosts
+	Some examples for Get Post
+
+Scenario: Get Post Test 1
+	Given I perform GET operation for "posts/{postid}"
+	Then I perform operation for post "1"
+	And I should see the "title" name as "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+
---
+++ ApiRestSharp/ApiRestSharp/Steps/GetPostsSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using RestSharp;
+using ApiRestSharp.Base;
+using ApiRestSharp.Model;
+using ApiRestSharp.Utilities;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace ApiRestSharp.Steps
+{
+    [Binding]
+    class GetPostsSteps
+    {
+        private Settings _settings;
+        public GetPostsSteps(Settings settings) => _settings = settings;
+        
+        [Given(@"I perform GET operation for ""(.*)""")]
+        public void GivenIPerformGETOperationFor(string url)
+        {
+            _settings.request = new RestRequest(url, Method.GET);
+        }
+
+        [Then(@"I perform operation for post ""(.*)""")]
+        public void ThenIPerformOperationForPost(int postId)
+        {
+            Thread.Sleep(2000);
+            _settings.request.AddUrlSegment("postid", postId.ToString());
+            _settings.response = _settings.restClient.ExecuteAsyncRequest<Posts>(_settings.request).GetAwaiter().GetResult();
+        }
+
+        [Then(@"I should see the ""(.*)"" name as ""(.*)""")]
+        public void ThenIShouldSeeTheNameAs(string key, string value)
+        {
+            Console.WriteLine();
+            Assert.That(_settings.response.GetResponseObject(key), Is.EqualTo(value), $"The {key} is not matching");
+       
+        }
+
+
+
+    }
+}
